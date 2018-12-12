61bf812afc6b562f9657deb08aac2d63b1afc087
Jiapei Kuang <jkuang6@ucsc.edu>
2018-12-12 02:02:19 -0800

    Update a post editing page

--- models/tables.py
+++ models/tables.py
@@ -8,7 +8,25 @@
 # Consult manual for more options, validators, etc.
 
 
+import datetime
 
+def get_user_email():
+    return None if auth.user is None else auth.user.email
 
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+db.define_table('post',
+                Field('post_author', default=get_user_email()),
+                Field('post_title'),
+                Field('post_content', 'text'),
+                Field('post_time', 'datetime', default=get_current_time()),
+                )
+
+# Likes. 
+db.define_table('user_like',
+                Field('user_email'), # The user who flagged
+                Field('post_id', 'reference post'), # The flagged post
+)
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
