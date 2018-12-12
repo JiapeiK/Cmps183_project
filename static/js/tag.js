fc3b503ee73d27fed7a8c42fd3ae55cf99c903c9
GeGeLu <44011574+GeGeLu@users.noreply.github.com>
2018-12-12 03:04:17 -0800

    Add files via upload

--- static/js/tag.js
+++ static/js/tag.js
@@ -213,3 +213,208 @@ var forest_clicks = 0;
     forest_clicks = forest_clicks + 1;
   }
 }
+
+var mother_clicks = 0;
+  function select_mother(_this) { 
+  if(mother_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    mother_clicks = mother_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    mother_clicks = mother_clicks + 1;
+  }
+}
+
+var father_clicks = 0;
+  function select_father(_this) { 
+  if(father_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    father_clicks = father_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    father_clicks = father_clicks + 1;
+  }
+}
+
+var grandparents_clicks = 0;
+  function select_grandparents(_this) { 
+  if(grandparents_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    grandparents_clicks = grandparents_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    grandparents_clicks = grandparents_clicks + 1;
+  }
+}
+
+var son_clicks = 0;
+  function select_son(_this) { 
+  if(son_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    son_clicks = son_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    son_clicks = son_clicks + 1;
+  }
+}
+
+
+var husband_clicks = 0;
+  function select_husband(_this) { 
+  if(husband_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    husband_clicks = husband_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    husband_clicks = husband_clicks + 1;
+  }
+}
+
+var wife_clicks = 0;
+  function select_wife(_this) { 
+  if(wife_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    wife_clicks = wife_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    wife_clicks = wife_clicks + 1;
+  }
+}
+
+var friends_clicks = 0;
+  function select_friends(_this) { 
+  if(friends_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    friends_clicks = friends_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    friends_clicks = friends_clicks + 1;
+  }
+}
+
+var lover_clicks = 0;
+  function select_lover(_this) { 
+  if(lover_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    lover_clicks = lover_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    lover_clicks = lover_clicks + 1;
+  }
+}
+
+var colleagues_clicks = 0;
+  function select_colleagues(_this) { 
+  if(colleagues_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    colleagues_clicks = colleagues_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    colleagues_clicks = colleagues_clicks + 1;
+  }
+}
+
+var figures_clicks = 0;
+  function select_figures(_this) { 
+  if(figures_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    figures_clicks = figures_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    figures_clicks = figures_clicks + 1;
+  }
+}
+
+var fantasy_clicks = 0;
+  function select_fantasy(_this) { 
+  if(fantasy_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    fantasy_clicks = fantasy_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    fantasy_clicks = fantasy_clicks + 1;
+  }
+}
+
+var celebrities_clicks = 0;
+  function select_celebrities(_this) { 
+  if(celebrities_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    celebrities_clicks = celebrities_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    celebrities_clicks = celebrities_clicks + 1;
+  }
+}
+
+var chased_clicks = 0;
+  function select_chased(_this) { 
+  if(chased_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    chased_clicks = chased_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    chased_clicks = chased_clicks + 1;
+  }
+}
+
+var toilet_clicks = 0;
+  function select_toilet(_this) { 
+  if(toilet_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    toilet_clicks = toilet_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    toilet_clicks = toilet_clicks + 1;
+  }
+}
+
+var flying_clicks = 0;
+  function select_flying(_this) { 
+  if(flying_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    flying_clicks = flying_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    flying_clicks = flying_clicks + 1;
+  }
+}
+
+var falling_clicks = 0;
+  function select_falling(_this) { 
+  if(falling_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    falling_clicks = falling_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    falling_clicks = falling_clicks + 1;
+  }
+}
+
+var late_clicks = 0;
+  function select_late(_this) { 
+  if(late_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    late_clicks = late_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    late_clicks = late_clicks + 1;
+  }
+}
