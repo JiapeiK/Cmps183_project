e418e8848a124439473295c77cb97dd7f183ef1e
GeGeLu <44011574+GeGeLu@users.noreply.github.com>
2018-12-11 19:04:46 -0800

    Add files via upload

---
+++ static/js/tag.js
@@ -0,0 +1,215 @@
+var school_clicks = 0;
+  function select_school(_this) { 
+  if(school_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    school_clicks = school_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    school_clicks = school_clicks + 1;
+  }
+}
+var air_clicks = 0;
+  function select_air(_this) {  
+  if(air_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    air_clicks = air_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    air_clicks = air_clicks + 1;
+  }
+}
+var vehicle_clicks = 0;
+  function select_vehicle(_this) {  
+  if(vehicle_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    vehicle_clicks = vehicle_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    vehicle_clicks = vehicle_clicks + 1;
+  }
+}
+var road_clicks = 0;
+  function select_road(_this) { 
+  if(road_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    road_clicks = road_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    road_clicks = road_clicks + 1;
+  }
+}
+var cave_clicks = 0;
+  function select_cave(_this) { 
+  if(cave_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    cave_clicks = cave_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    cave_clicks = cave_clicks + 1;
+  }
+}
+
+var trapped_clicks = 0;
+  function select_trapped(_this) { 
+  if(trapped_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    trapped_clicks = trapped_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    trapped_clicks = trapped_clicks + 1;
+  }
+}
+
+var house_clicks = 0;
+  function select_house(_this) { 
+  if(house_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    house_clicks = house_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    house_clicks = house_clicks + 1;
+  }
+}
+
+var stairs_clicks = 0;
+  function select_stairs(_this) { 
+  if(stairs_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    stairs_clicks = stairs_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    staris_clicks = stairs_clicks + 1;
+  }
+}
+
+var bridge_clicks = 0;
+  function select_bridge(_this) { 
+  if(bridge_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    bridge_clicks = bridge_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    bridge_clicks = bridge_clicks + 1;
+  }
+}
+
+var boat_clicks = 0;
+  function select_boat(_this) { 
+  if(boat_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    boat_clicks = boat_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    boat_clicks = boat_clicks + 1;
+  }
+}
+
+
+var water_clicks = 0;
+  function select_water(_this) { 
+  if(water_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    water_clicks = water_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    water_clicks = water_clicks + 1;
+  }
+}
+
+var hospital_clicks = 0;
+  function select_hospital(_this) { 
+  if(hospital_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    hospital_clicks = hospital_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    hospital_clicks = hospital_clicks + 1;
+  }
+}
+
+
+var mountains_clicks = 0;
+  function select_mountains(_this) { 
+  if(mountains_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    mountains_clicks = mountains_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    mountains_clicks = mountains_clicks + 1;
+  }
+}
+
+
+var bar_clicks = 0;
+  function select_bar(_this) { 
+  if(bar_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    bar_clicks = bar_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    bar_clicks = bar_clicks + 1;
+  }
+}
+
+
+var prom_clicks = 0;
+  function select_prom(_this) { 
+  if(prom_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    prom_clicks = prom_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    prom_clicks = prom_clicks + 1;
+  }
+}
+
+var church_clicks = 0;
+  function select_church(_this) { 
+  if(church_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    church_clicks =+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    church_clicks = church_clicks + 1;
+  }
+}
+
+var circus_clicks = 0;
+  function select_circus(_this) { 
+  if(circus_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    circus_clicks = circus_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    circus_clicks = circus_clicks + 1;
+  }
+}
+
+var forest_clicks = 0;
+  function select_forest(_this) { 
+  if(forest_clicks%2 == 0){
+    _this.style.backgroundColor = "red";
+    forest_clicks = forest_clicks+1;
+  }
+  else {
+    _this.style.backgroundColor = "gray";
+    forest_clicks = forest_clicks + 1;
+  }
+}
